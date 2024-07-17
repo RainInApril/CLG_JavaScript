8e393998f02e0007e21ebb349523a5c6d25be72c
Gem Phan <90890779+RainInApril@users.noreply.github.com>
2024-07-17 18:52:51 +1000

    Complete assignment 6

---
+++ javascript_6.js
@@ -0,0 +1,30 @@
+function changeHeadingColor() {
+    const headingElement = document.getElementById('heading');
+    headingElement.style.color = 'red';
+}
+
+function changeParagraphContent() {
+    const paragraphElement = document.getElementById('paragraph');
+    paragraphElement.innerHTML = 'The content has changed!';
+}
+
+function showHiddenContent() {
+    const hiddenElement = document.querySelector('.hidden');
+    hiddenElement.classList.remove('hidden');
+}
+
+const heading = document.getElementById('heading');
+heading.addEventListener('mouseover', function () {
+    this.classList.add('highlighted');
+});
+
+heading.addEventListener('mouseout', function () {
+    this.classList.remove('highlighted');
+});
+
+const changeColorButton = document.getElementById('change-color');
+changeColorButton.addEventListener('click', changeHeadingColor);
+
+changeColorButton();
+changeHeadingColor();
+changeParagraphContent();
